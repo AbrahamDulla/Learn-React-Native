c1bf1720a7193f394393b8da2611b86bb03ae473
AbrahamDulla <abrish.d143t@gmail.com>
2024-03-31 22:53:34 +0300

    modification on Exercise Screen

--- app/components/screens/Exercises.jsx
+++ app/components/screens/Exercises.jsx
@@ -5,47 +5,48 @@ import {
   Text,
   TouchableOpacity,
   View,
+  ImageBackground,
 } from 'react-native';
 
 const exercises = [
   {
     name: 'chest',
-    image: 'image',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Back',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Shoulder',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Leg',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Biceps',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Triceps',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Abs',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
   {
-    name: 'chest',
-    image: 'image',
+    name: 'Cardio',
+    image: require('../../assets/images/login/login1.jpg'),
     navigateTo: 'nativgate to',
   },
 ];
@@ -53,9 +54,29 @@ const exercises = [
 export default function Exercises() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      <TouchableOpacity
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: 100,
+          width: '100%',
+          backgroundColor: 'blue',
+          padding: 10,
+          borderRadius: 10,
+        }}>
+        <Text style={{color: 'white', fontSize: 18}}>
+          Ads Please subscribe the premiem package for Ads free
+        </Text>
+      </TouchableOpacity>
       {exercises.map((exercise, index) => (
-        <TouchableOpacity style={styles.card} key={index}>
-          <Text>{exercise.name}</Text>
+        <TouchableOpacity>
+          <ImageBackground
+            style={styles.backgroundImage}
+            source={exercise.image}
+            key={index}>
+            <Text style={{color: 'white', padding: 10}}>{exercise.name}</Text>
+          </ImageBackground>
         </TouchableOpacity>
       ))}
     </ScrollView>
@@ -67,14 +88,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 30,
+    padding: 20,
     flexWrap: 'wrap',
-    gap: 25,
+    gap: 20,
   },
-  card: {
-    width: '46%',
+  backgroundImage: {
+    width: 160,
     height: 150,
-    backgroundColor: 'blue',
-    textColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
   },
 });
